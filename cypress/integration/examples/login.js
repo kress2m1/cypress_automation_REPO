266154e1598e4636a43fa86be93ab91b1e5e4d78
andre dada <kress2m1@gmail.com>
2023-06-15 21:32:45 +0100

    created new user account and login file

--- cypress/integration/examples/login.js
+++ cypress/integration/examples/login.js
@@ -1,1 +1,14 @@
 /// <reference types="cypress" />
+
+describe("Create a user account at ParaBank", function() {
+    it("Navigate to the landing page and creates a new account", function() {
+        cy.visit("https://parabank.parasoft.com/parabank/index.htm")
+
+        cy.get("input[name='username']").type("Andre3000")
+        cy.get("input[name='password']").type("password1234")
+        cy.get("input[value='Log In']").click()
+      
+        cy.contains("Andre Dada").should("contain.text", "Andre Dada")
+
+    })
+})
